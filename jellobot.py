cb6f6034f554b53785c21d296f886b1deb58c5e4
rachitkakkar <71226802+rachitkakkar@users.noreply.github.com>
2021-03-25 12:02:27 -0500

    Update jellobot.py

--- jellobot.py
+++ jellobot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 import re
 import random
 
+bank = {}
+
 client = commands.Bot(command_prefix='?')
 TOKEN = ''
 
@@ -47,9 +49,17 @@ async def on_message(message):
                 if chance <= 50:
                     await message.channel.send(f'your face is {regex[0]}')
 
+        if "poop" in message.content:
+            if chance <= 50:
+                await message.channel.send('Poop??? HAHAHAHHAHAHAHAHh :rofl::rofl: :rofl: :rofl: :rofl: :rofl: https://media.discordapp.net/attachments/777581776217440307/819984348580937778/explode.gif')
+        
+        if "cool" in message.content:
+            if chance <= 50:
+                await message.channel.send('i bet this is cooler https://cdn.discordapp.com/attachments/810275293839097926/824673855259672616/maxresdefault.png')
+        
         if "bruh" in message.content:
             if chance <= 50:
-                await message.channel.send(f'burh')
+                await message.channel.send('burh')
 
     await client.process_commands(message)
 
@@ -70,6 +80,36 @@ async def ping(ctx):
 @client.command(name='green_screen', help='This command helps you frame me')
 async def green_screen(ctx):
     await ctx.send('https://i.ytimg.com/vi/80ygBP8r7As/maxresdefault.jpg')
+
+@client.command(name='create_account', help='This command gives you a bank account with $1,000')
+async def create_account(ctx):
+    global bank
+
+    if ctx.author.name in bank:
+        await ctx.send('Hey, you already have a bank account!')
+
+    else:
+        bank.update({ctx.author.name: 1000})
+        await ctx.send('Account has been created, and $1,000 has been placed in it!')
+
+@client.command(name='work', help='This command gives you money ranging from 1-100')
+async def work(ctx):
+    global bank
+
+    if ctx.author.name in bank:
+        money = random.randint(1, 100)
+        bank[ctx.author.name] += money
+
+        await ctx.send(f'You earned ${money}')
+
+    else:
+        await ctx.send("You don't have a bank account")
+
+@client.command(name='lb', help='See leadboard')
+async def lb(ctx):
+    global bank
+
+    await ctx.send(str(bank))
 
 @client.event
 async def on_guild_join(guild):
